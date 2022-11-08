cf6d480769534b93d36e9bb30058723b9d6584ea
Martin Wrodarczyk <mw@web-computing.de>
2022-11-08 19:10:13 +0100

    Add missing java doc

--- src/main/java/io/github/mwrod453/LibraryLicenseException.java
+++ src/main/java/io/github/mwrod453/LibraryLicenseException.java
@@ -1,5 +1,8 @@
 package io.github.mwrod453;
 
+/**
+ * Thrown when the license of a library can't be retrieved.
+ */
 public class LibraryLicenseException extends Exception {
 
     LibraryLicenseException(String message) {
